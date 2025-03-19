771c09d8899fb90af70c2e496b6d44517e369e7c
enkoding1 <ksjdevelop53@gmail.com>
2025-03-19 23:28:17 +0900

    chore: bonus

--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -1,6 +1,29 @@
+#include "pipex.h"
 
-int main(void)
+void	child_process(int *fd, char **argv, char **envp)
 {
-	
+	int fd[2];
+	pid_t pid;
+
+	pipe(fd);
+	pid = fork();
+
+	if (pid == 0)
+	{
+		
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+	}
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	if(argc >= 5)	
+	{
+		// if (ft_strncmp(argv[1], "here_doc"))	
+		
+	}
 	return 0;
 }
